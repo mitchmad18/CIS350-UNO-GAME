1eff202b14441eda43eed882ccb3f74f84e391c5
Myren <51705901+mitchmad18@users.noreply.github.com>
2019-10-23 17:22:26 -0400

    added some rules to popup dialog

--- src/UnoGui.java
+++ src/UnoGui.java
@@ -4,12 +4,19 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class UnoGui implements ActionListener {
+
+    //Variables for the GUI
     private JFrame f;
     private JMenuBar mb;
     private JMenu file;
     private JMenuItem reset, exit;
     private JButton btnDrawCard;
     private JPanel panel;
+
+    //Variables for the rules dialog box
+    private Integer[] optionsNumOfCardsDealt = new Integer[] {5,6,7,8,9,10};
+    private SpinnerListModel sLMNumOfCardsDealt = new SpinnerListModel(optionsNumOfCardsDealt);
+    private JSpinner numOfCardsDealt = new JSpinner(sLMNumOfCardsDealt);
 
     private UnoGui(){
         f=new JFrame("Uno Game");
@@ -47,6 +54,16 @@ public class UnoGui implements ActionListener {
         f.getContentPane().add(panel);
         f.setVisible(true);
 
+        JRadioButton yesButton = new JRadioButton("Yes");
+        yesButton.setSelected(false);
+        JRadioButton noButton = new JRadioButton("No");
+        noButton.setSelected(true);
+
+        ButtonGroup stackDraw2 = new ButtonGroup();
+        stackDraw2.add(yesButton);
+        stackDraw2.add(noButton);
+
+
         resetGame();
     }
 
@@ -64,13 +81,26 @@ public class UnoGui implements ActionListener {
 
     //Resets the gameboard and prompts the rules popup menu
     private void resetGame() {
-        JOptionPane.showMessageDialog(f,
-                "Welcome to UNO!\nPlease select the rules you would like to use: ",
-                "New Game",
-                JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showConfirmDialog(null,
+                dialogOptions, "My custom dialog", JOptionPane.PLAIN_MESSAGE);
+
     }
 
     private void drawCard() {
 
     }
+
+
+    JRadioButton noStack = new JRadioButton("No");
+
+    ButtonGroup stackDraw2Options = new ButtonGroup();
+
+    //Sets the options for the rules dialog box
+    private final JComponent[] dialogOptions = new JComponent[]{
+            new JLabel("Welcome to UNO!"),
+            new JLabel("Please select the rules you would like to use:"),
+            new JLabel("\nHow many cards should be dealt?"),
+                numOfCardsDealt,
+            new JLabel("Would you like to allow Draw 2 cards to be stacked?")
+    };
 }
